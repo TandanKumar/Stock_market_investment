f16ff60b27d64fb6b4eba49699f5990be06937ac
TandanKumar <tandankumar90@gmail.com>
2023-07-04 12:27:41 +0530

    updating the skelton of the project

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Stock_market_investment
+
+## This project about the investment in the share market.
+
+# Creating the conda env
+conda create -p venv python==3.7 -y
+
+------------------------------
+
+# To activate the conda env 
+conda activate venv
+
+    or
+
+conda activate .clear\venv
+
+----------------------------------
+
+# for creating the skelton of the project used a tempelate file 
+
+ Here command is written to create all the files and dolders
+
+ ---------------------------------------------
---
+++ tempelate.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+package_name = "housing"
+
+list_of_files = [
+
+ "requirements.txt",
+ "app.py",
+ "Dockerfile",
+ ".dockerignore",
+ ".github\workflows\main.yaml",
+ "setup.py",
+  f"{package_name}/__init__.py", 
+   f"{package_name}/exception/__init__.py",
+   f"{package_name}/logger/__init__.py",
+   f"{package_name}/pipeline/__init__.py",
+   f"{package_name}/config/__init__.py",
+   f"{package_name}/entity/__init__.py",
+   f"{package_name}/components/__init__.py"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
+            
